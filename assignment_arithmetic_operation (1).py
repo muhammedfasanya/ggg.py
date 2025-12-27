336a595e5ddd9671c6d6074b65d5e137ce2f7ac7
muhammedfasanya <muhammedofasanya@gmail.com>
2025-12-27 18:40:04 -0330

    assignment_arithmetic_operation (1).py

---
+++ assignment_arithmetic_operation (1).py
@@ -0,0 +1,33 @@
+#Arithmetic Operations in Python
+#Wrrite a pthon code for each of these and include the output
+
+#Exercise 1: Calculate the result of 24 + 16 - 5
+#Solution: 35
+
+#Exercise 2: Multiply 12 by 9 and divide the result by 3
+#Solution: 36
+
+#Exercise 3: Find the remainder when 89 is divided by 6
+#Solution: 5
+
+#Exercise 4: Perform floor division of 125 by 8
+#Solution: 15
+
+#Exercise 5: Calculate 4 raised to the power of 5
+#Solution:1024
+#Exercise 6: Evaluate: 20 + 15 * 2 - 10
+#Solution: 40
+
+#Exercise 7: Evaluate using parentheses: (20 + 15) * (2 - 1)
+#Solution: 35
+
+#Exercise 8: Convert 135 minutes into hours and minutes
+#Solution: 2.25
+
+#Exercise 9: Find the average of 10, 20, and 30
+#Solution: 20
+
+#Exercise 10:You have 535 seconds. Use Python arithmetic to convert this into minutes and remaining seconds.
+#Hint: Use // for the minutes and % for the remaining seconds.
+#Solution: 8 minutes and 55 seconds.
+
