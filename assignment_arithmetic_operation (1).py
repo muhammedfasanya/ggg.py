77e92c01d8c7139ac562300c67e8587c95e22033
muhammedfasanya <muhammedofasanya@gmail.com>
2025-12-28 15:32:54 -0330

    change 1

--- assignment_arithmetic_operation (1).py
+++ assignment_arithmetic_operation (1).py
@@ -2,32 +2,40 @@
 #Wrrite a pthon code for each of these and include the output
 
 #Exercise 1: Calculate the result of 24 + 16 - 5
-#Solution: 35
+#Solution:24+16-5
+# 35
 
 #Exercise 2: Multiply 12 by 9 and divide the result by 3
-#Solution: 36
+#Solution:12*9/3
+#  36
 
 #Exercise 3: Find the remainder when 89 is divided by 6
-#Solution: 5
+#Solution: 86%6
+# 5
 
 #Exercise 4: Perform floor division of 125 by 8
-#Solution: 15
+#Solution: # 125//8
+# 5
 
 #Exercise 5: Calculate 4 raised to the power of 5
-#Solution:1024
+#Solution:4**5
+# 1024
 #Exercise 6: Evaluate: 20 + 15 * 2 - 10
-#Solution: 40
+#Solution:  20 + 15 * 2 - 10
+# 40
 
 #Exercise 7: Evaluate using parentheses: (20 + 15) * (2 - 1)
-#Solution: 35
+#Solution: (20 + 15) * (2 - 1)
+#  35
 
 #Exercise 8: Convert 135 minutes into hours and minutes
-#Solution: 2.25
+#Solution: 135//60 gives you 2
+ #Solution:  135%60 gives you 15
 
 #Exercise 9: Find the average of 10, 20, and 30
 #Solution: 20
 
 #Exercise 10:You have 535 seconds. Use Python arithmetic to convert this into minutes and remaining seconds.
 #Hint: Use // for the minutes and % for the remaining seconds.
-#Solution: 8 minutes and 55 seconds.
-
+#Solution:  535 // 60 = 8
+#Solution:  535 % 60 = 55
